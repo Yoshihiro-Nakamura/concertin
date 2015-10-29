a50709d876e7a572e4cec81443531e20844eeabb
yoshihironakamura <ynakamura.vla@gmail.com>
2015-10-29 20:37:09 +0900

    動画用APIのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-
   root 'home#index'
+
+  namespace :api, defaults: {format: :json} do
+    resources :song_groups, only: [:index] do
+      collection do
+        get 'search'
+      end
+    end
+  end
+
 end
