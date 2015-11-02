00929ee032453bd820c1e4e1595a5b65e875e94a
yoshihironakamura <ynakamura.vla@gmail.com>
2015-11-03 01:14:00 +0900

    直近１ヶ月のコンサート情報を取得するようにした。

--- app/controllers/api/song_groups_controller.rb
+++ app/controllers/api/song_groups_controller.rb
@@ -1,6 +1,8 @@
 class Api::SongGroupsController < ApplicationController
   def index
-    songs = Song.arr_video_id.shuffle
+    from = DateTime.now
+    to = from + 1.month
+    songs = Concert.joins("INNER JOIN songs ON concerts.main_song_id = songs.id").where(start_datetime: from .. to).order(start_datetime: :desc).pluck(:video_id).shuffle
     render json: songs
   end
 end
