6606c579bf84fff12d5348cfd6a03f56e160e088
yoshihironakamura <ynakamura.vla@gmail.com>
2015-11-03 01:08:13 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :concerts do
+    collection do
+      get 'search'
+    end
+  end
+
   namespace :api, defaults: {format: :json} do
     resources :song_groups, only: [:index]
   end
