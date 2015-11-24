88d41caabf39fa992b3b0bc7d6eae7ce31ba4935
yoshihironakamura <ynakamura.vla@gmail.com>
2015-11-25 00:22:26 +0900

    ユーザーテスト用に、曲が被らないようにした

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,6 +2,6 @@ class ConcertsController < ApplicationController
   def search
     from = DateTime.now
     to = from + 1.month
-    @concerts = Song.find_by(video_id: params[:videoId]).concerts.where(start_datetime: from .. to).shuffle
+    @concerts = Song.find_by(video_id: params[:videoId]).concerts.where(start_datetime: from .. to).shuffle.uniq
   end
 end
