40e5c01fa7ff493bf897f7628017d54d62ed87e9
yoshihironakamura <ynakamura.vla@gmail.com>
2016-02-08 11:44:46 +0900

    フォーム画面、機能の追加

--- app/controllers/api/concerts_controller.rb
+++ app/controllers/api/concerts_controller.rb
@@ -9,4 +9,35 @@ class Api::ConcertsController < ApplicationController
     @concert = Concert.find(params[:id])
   end
 
+  def get_hall
+    @halls = Hall.where('name LIKE(?)', "%#{params[:term]}%").pluck(:name)
+  end
+
+  def get_conductor
+    @conductors = Conductor.where('name LIKE(?)', "%#{params[:term]}%").pluck(:name)
+  end
+
+  def get_concert_title
+    @concert_titles = Concert.where('title LIKE(?)', "%#{params[:term]}%").pluck(:title)
+  end
+
+  def get_artist
+    @artists = Artist.where('name LIKE(?)', "%#{params[:term]}%").pluck(:name)
+  end
+
+  def get_contact
+    @contacts = Contact.where('name LIKE(?)', "%#{params[:term]}%").pluck(:name)
+  end
+
+  def get_instrument
+    @instruments = Instrument.all
+  end
+
+  def get_composer
+    @composers = Composer.where('name LIKE(?)', "%#{params[:term]}%").pluck(:name)
+  end
+
+  def get_song
+    @songs = Song.where('title LIKE(?)', "%#{params[:term]}%").pluck(:title)
+  end
 end
