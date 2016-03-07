8b29eeebfb61c0c2c03d074fd5d57c932878f5e9
yoshihironakamura <ynakamura.vla@gmail.com>
2016-03-08 01:55:04 +0900

    uniq!だと重複がない場合にnilになってしまうので!をとった。

--- app/controllers/api/song_groups_controller.rb
+++ app/controllers/api/song_groups_controller.rb
@@ -2,7 +2,7 @@ class Api::SongGroupsController < ApplicationController
   def index
     from = DateTime.now
     to = from + 1.month
-    songs = Concert.joins("INNER JOIN songs ON concerts.main_song_id = songs.id").where(start_datetime: from .. to).order(start_datetime: :desc).pluck(:video_id).shuffle.uniq!
+    songs = Concert.joins("INNER JOIN songs ON concerts.main_song_id = songs.id").where(start_datetime: from .. to).order(start_datetime: :desc).pluck(:video_id).shuffle.uniq
     render json: songs
   end
 end
